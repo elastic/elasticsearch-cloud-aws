169cde2c7c31086c02eba569c321ff7921b4b9b7
Robert Muir <rmuir@apache.org>
2015-05-08 12:41:43 -0400

    Disable response metadata cache in this ClientConfiguration as well.

--- src/main/java/org/elasticsearch/cloud/aws/InternalAwsS3Service.java
+++ src/main/java/org/elasticsearch/cloud/aws/InternalAwsS3Service.java
@@ -93,6 +93,9 @@ public class InternalAwsS3Service extends AbstractLifecycleComponent<AwsS3Servic
         }
 
         ClientConfiguration clientConfiguration = new ClientConfiguration();
+        // the response metadata cache is only there for diagnostics purposes,
+        // but can force objects from every response to the old generation.
+        clientConfiguration.setResponseMetadataCacheSize(0);
         if (protocol == null) {
             protocol = componentSettings.get("protocol", "https").toLowerCase();
             protocol = componentSettings.get("s3.protocol", protocol).toLowerCase();
